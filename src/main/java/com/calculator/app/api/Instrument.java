1b7f6c4da4b46ed0097568581afdf1382a659793
kishchavda <kishan.chavda@hotmail.co.uk>
2017-05-04 14:22:43 +0100

    Updating enums

--- src/main/java/com/calculator/app/api/Instrument.java
+++ src/main/java/com/calculator/app/api/Instrument.java
@@ -2,5 +2,9 @@ package com.calculator.app.api;
 
 public enum Instrument {
     INSTRUMENT0,
+    INSTRUMENT1,
+    INSTRUMENT2,
+    INSTRUMENT3,
+    //...
     INSTRUMENT19
 }
--- src/main/java/com/calculator/app/api/Market.java
+++ src/main/java/com/calculator/app/api/Market.java
@@ -2,5 +2,8 @@ package com.calculator.app.api;
 
 public enum Market {
     MARKET0,
-    MARKET1, MARKET49
+    MARKET1,
+    //...
+    MARKET48,
+    MARKET49
 }
