aa8ce0569227e87891031a6808316c40212bfe9c
kishchavda <kishan.chavda@hotmail.co.uk>
2017-05-06 23:13:56 +0100

    Removing unused entities and refactoring

--- src/main/java/com/calculator/app/api/MarketUpdateImpl.java
+++ src/main/java/com/calculator/app/api/MarketUpdateImpl.java
@@ -1,20 +1,9 @@
 package com.calculator.app.api;
 
-public class MarketUpdateImpl implements MarketUpdate {
+import lombok.Value;
 
+@Value
+class MarketUpdateImpl implements MarketUpdate {
     private final Market market;
     private final TwoWayPrice twoWayPrice;
-
-    public MarketUpdateImpl(Market market, TwoWayPrice twoWayPrice) {
-        this.market = market;
-        this.twoWayPrice = twoWayPrice;
-    }
-
-    public Market getMarket() {
-        return market;
-    }
-
-    public TwoWayPrice getTwoWayPrice() {
-        return twoWayPrice;
-    }
 }
